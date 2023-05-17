3975642b84eb90c9c2d7a7a8d764ca443dfba484
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-05-17 13:34:39 +0300

    двух фазовый конструктор

--- src/main/java/com/philimonov/quoters/TerminatorQuoter.java
+++ src/main/java/com/philimonov/quoters/TerminatorQuoter.java
@@ -1,10 +1,20 @@
 package com.philimonov.quoters;
 
 public class TerminatorQuoter implements Quoter {
-    @InjectRandomInt(min=2, max=7)
+    @InjectRandomInt(min = 2, max = 7)
     private int repeat;
 
     private String message;
+
+    public void init() {
+        System.out.println("Phase 2");
+        System.out.println("repeat = " + repeat);
+    }
+
+    public TerminatorQuoter() {
+        System.out.println("Phase 1");
+        System.out.println("repeat = " + repeat);
+    }
 
     public String getMessage() {
         return message;
