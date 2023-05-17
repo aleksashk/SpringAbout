302c5518062c05252f36edef2760c8bd98333a58
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-05-17 13:24:04 +0300

    Initial commit

---
+++ src/main/java/com/philimonov/Main.java
@@ -0,0 +1,12 @@
+package com.philimonov;
+
+import com.philimonov.quoters.TerminatorQuoter;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+public class Main {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
+        context.getBean(TerminatorQuoter.class).sayQuote();
+    }
+}
---
+++ src/main/java/com/philimonov/quoters/Quoter.java
@@ -0,0 +1,5 @@
+package com.philimonov.quoters;
+
+public interface Quoter {
+    void sayQuote();
+}
